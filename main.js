e27140326ca6d515df749d3fa8ca5e595d78bf5d
Tom Gregan <tgregan90@hotmail.co.uk>
2020-09-29 14:54:58 +0100

    Add export function

--- main.js
+++ main.js
@@ -3,21 +3,20 @@ async function getAndPrint(url) {
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         dataObj = data;
       }).catch(error => console.log("error is +=" + error));
-      console.table(`Returned: ${dataObj.Title}`)
       return dataObj
   }
+let arr = [];
 async function getVals(e){
       e.preventDefault();
+      arr = [];
       const apiKey = getApiKeyFromInput();
       if(apiKey.toString() != "Error"){
           printHeadings();
           let movies = document.querySelector("#userMovies").value.split("\n");
           asyncForEach(movies,async (movie) => {
               let movieObj = await getAndPrint("https://www.omdbapi.com/?apikey="+ apiKey+ "&t=" + movie.toString().toLowerCase().replace(" ","+"));
-            console.log(movieObj.Title)
               await printObj(movieObj);
           });
       }
@@ -25,13 +24,21 @@ async function getVals(e){
   }
 function printObj(obj){
     let div = document.createElement("div");
+    let movieData = {};
+    movieData.MovieTitle = obj.Title;
+    movieData.Year = obj.Year
+    movieData.Released = obj.Released;
+    movieData.Director = obj.Director.replace(","," ").toString();
+    movieData.Actors = obj.Actors.replace(","," ").toString();
+    movieData.Actors = movieData.Actors.replace(","," ").toString();
+    arr.push(movieData);
     div.id = "row";
-    let message = `<p>${obj.Title},${obj.Year},${obj.Released},${obj.Director},${obj.Actors},</p>`;  
+    document.body.classList.add("results");
+    let message = `<p> ${obj.Title},${obj.Year},${obj.Released},${obj.Director.replace(","," ")},${obj.Actors.replace(","," ")} </p>`;  
     div.innerHTML = message;
     console.log(message);
-    document.querySelector("#results").append(div)  ;
+    document.querySelector("#results").innerHTML += message  ;
   }
-
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
       await callback(array[index], index, array);
@@ -51,8 +58,49 @@ function getApiKeyFromInput(){
       }
       return inputVal
   }
-
 function printHeadings(){
     document.querySelector("#results").innerHTML = `<h2>Movie Name, Year, Released,Directors,Actors,</h2>`;
+    const headings =    {};
+    headings.title = "Movie Title";
+    headings.year = "Year";
+    headings.Released = "Released Date";
+    headings.Director = "Director";
+    headings.Actors = "Actors"
+  arr.push(headings);
+
   }
+function downloadResults(){
+  exportToCsv("data.csv",arr);
+}
+function exportToCsv(filename, items) {
+  let csv
+  // Loop the array of objects
+  for(let row = 0; row < items.length; row++){
+      let keysAmount = Object.keys(items[row]).length
+      let keysCounter = 0
+
+      // If this is the first row, generate the headings
+      if(row === 0){
+        // Loop each property of the object
+        for(let key in items[row]){
+          csv += key + (keysCounter+1 < keysAmount ? ',' : '\r\n' )
+          keysCounter++
+        }
+      }else{
+        for(let key in items[row]){
+            csv += items[row][key] + (keysCounter+1 < keysAmount ? ',' : '\r\n' )
+            keysCounter++
+        }
+      }
+
+      keysCounter = 0
+  }
+let link = document.createElement('a')
+link.id = 'download-csv'
+link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(csv));
+link.setAttribute('download', 'yourfiletextgoeshere.csv');
+document.body.appendChild(link)
+document.querySelector('#download-csv').click()
+}
 document.querySelector("#GoButton").addEventListener("click",getVals);
+document.querySelector("#downloadButton").addEventListener("click",downloadResults);
