069613b670f64ab1f3efc3e7c993c332149f3519
Tom Gregan <tgregan90@hotmail.co.uk>
2020-09-29 11:41:30 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,53 @@
+async function getAndPrint(url) {
+    let dataObj = {};
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        dataObj = data;
+      }).catch(error => console.log("error is +=" + error));
+      console.table(`Returned: ${dataObj.Title}`)
+      return dataObj
+  }
+async function getVals(e){
+        printHeadings();
+      e.preventDefault();
+      const apiKey = getApiKeyFromInput();
+      let movies = document.querySelector("#userMovies").value.split("\n");
+
+    asyncForEach(movies,async (movie) => {
+        let movieObj = await getAndPrint("https://www.omdbapi.com/?apikey="+ apiKey+ "&t=" + movie.toString().toLowerCase().replace(" ","+"));
+       console.log(movieObj.Title)
+        await printObj(movieObj);
+    });
+  }
+function printObj(obj){
+    let div = document.createElement("div");
+    div.id = "row";
+    let message = `<img src="${obj.Poster}" id="profileImage"/><p>${obj.Title},${obj.Year},${obj.Released},${obj.Director},${obj.Actors},</p>`;  
+    div.innerHTML = message;
+    console.log(message);
+    document.querySelector("#results").append(div)  ;
+  }
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+function getApiKeyFromInput(){
+    const inputVal = document.getElementById("apiKey").value;
+      if(inputVal==""){
+          alert("Please include an API Key");
+          return
+      }
+      if(inputVal.length != 8){
+          alert("Your APIKey Should be 8 chars long.");
+      }
+      return inputVal
+  }
+
+function printHeadings(){
+    document.querySelector("#results").innerHTML = `<h2>Movie Name, Year, Released,Directors,Actors,</h2>`;
+  }
+document.querySelector("#GoButton").addEventListener("click",getVals);
